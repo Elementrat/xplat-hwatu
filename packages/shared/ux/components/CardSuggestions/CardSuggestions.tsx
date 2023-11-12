03be9aac0ebcecfec5b91d2c1b1e5d11cbf7863e
elementrat <elementrat@gmail.com>
2023-11-11 18:17:40 -0600

    working translation

--- packages/shared/ux/components/CardSuggestions/CardSuggestions.tsx
+++ packages/shared/ux/components/CardSuggestions/CardSuggestions.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import styles from "./CardSuggestions.module.css";
+import { TranslatorCredential } from "@azure-rest/ai-translation-text";
+import createClient from "@azure-rest/ai-translation-text";
 
 const DELAY = 2000;
 
@@ -9,7 +11,22 @@ const CardSuggestions = ({ inputText }) => {
   const [timeoutID, setTimeoutID] = useState();
 
   const tryGetSuggestions = async () => {
-    console.log("__WOW");
+    const translateCedential: TranslatorCredential = {
+      key: "85b9501d87ce4de3a2315e6359ad52e7",
+      region: "westus2"
+    };
+
+    const translationClient = await createClient(
+      "https://api.cognitive.microsofttranslator.com/",
+      translateCedential
+    );
+
+    const translateResponse = await translationClient.path("/translate").post({
+      body: [{ text: inputText }],
+      queryParameters: { to: "ko", from: "en" }
+    });
+
+    console.log("__TRANSLATION_RESPONSE", translateResponse);
   };
 
   useEffect(() => {
